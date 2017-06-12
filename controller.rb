b5a272f20d5d3cb663db61a64e2779067a8808f8
James Cox <j-cox03@hotmail.com>
2017-06-13 00:49:03 +0100

    Added css file for result page

--- controller.rb
+++ controller.rb
@@ -12,9 +12,8 @@ get('/game') do
 end
 
 get("/result/:hand1/:hand2") do
-
   hand1 = RockPaperScissors.new(params[:hand1])
   hand2 = RockPaperScissors.new(params[:hand2])
-
+  @result = hand1.play_against(hand2)
   erb(:result)
 end
